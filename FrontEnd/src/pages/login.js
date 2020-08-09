fe31ab655d9c7ea5d918429e7b95c0d09d619106
Duc <duc.h@hotmail.com>
2020-08-10 02:16:39 +1000

    added state for login page, have fetch post ready

--- FrontEnd/src/pages/login.js
+++ FrontEnd/src/pages/login.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/login.css';
 
 const Login = () => {
-    const handleSubmit = (loginInfo) => {
-        alert(`Submitted: ${JSON.stringify(loginInfo)}`)
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const setEmailState = (newValue) => {
+        setEmail(newValue.target.value);
+    }
+
+    const setPasswordState = (newValue) => {
+        setPassword(newValue.target.value);
+    }
+
+    const handleSubmit = async (loginInfo) => {
+        alert(`Submittedddd: ${email} and ${password}`);
+
+        var data = {
+
+        };
+
+        // use whatever the springboot url is
+        const response = await fetch('localhost:3001/authenticate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
     }
 
     return(
         <div id="loginContainer">
             <h1 id="header">Login</h1>
             <div id="formArea">
-                <form id="loginForm" onSubmit={handleSubmit} method="POST">
-                    <input name="email" type="email" placeholder=" Email"/>
-                    <input name="password" type="password" placeholder=" Password"/>
-                    <button id="submitBtn" type="submit">Login</button>
+                <form id="loginForm" onSubmit={handleSubmit}>
+                    <input className="loginInputField" name="email" type="email" placeholder=" Email" onChange={setEmailState}/>
+                    <input className="loginInputField" name="password" type="password" placeholder=" Password" onChange={setPasswordState}/>
+                    <input className="submitBtn" type="button" value="Login" onClick={handleSubmit}/>
                     <p>Don't have an account? <Link to="/signup">Register here</Link></p>
                 </form>
             </div>
