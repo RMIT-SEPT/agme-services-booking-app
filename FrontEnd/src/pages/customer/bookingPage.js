2f68bbe6b0b357ce6e5c559175b3f332fb48ecf2
[Marco Barros s3379774] <s3379774@student.rmit.edu.au>
2020-08-31 17:10:35 +1000

    change calendar type and basic setup

--- FrontEnd/src/pages/customer/bookingPage.js
+++ FrontEnd/src/pages/customer/bookingPage.js
@@ -1,18 +1,54 @@
 import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import '../../css/pages/bookingPage.css';
 
-const BookingPage = (userJSON) => {
+const BookingPage = (userDetails) => {
+    const userName = userDetails.userName;
+    const localizer = momentLocalizer(moment)
+    const currentDate = new Date();
     
-    const [date, setDate] = useState(new Date());
+    const myEventsList = [{
+        title: "hello",
+        start: currentDate,
+        end: currentDate,
+        allDay: false,
+        resource: null
+    }, 
+    {
+        title: "Fridge",
+        start: new Date('September 2, 2020 03:00:00'),
+        end: new Date('September 2, 2020 04:00:00'),
+        allDay: false,
+        resource: {
+            worker: "Dane Swan",
+            description: "AAAAAAAAAAAA"
+        }
+    },
+    {
+        title: "Lemon",
+        start: new Date('September 1, 2020 01:00:00'),
+        end: new Date('September 1, 2020 01:30:00'),
+        allDay: false,
+        resource: {
+            worker: "Mylie Cyrus",
+            description: "b"
+        }
+    }]
 
-    const onChange = date => {
-        setDate(date)
-    }
-    
     return(
         <div id="bookings">
-            <Calendar onChange={onChange} value={date}/>
+            <Calendar
+                localizer={localizer}
+                events={myEventsList}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 400, width: 750}}
+                defaultView={'work_week'}
+                views={['work_week', 'day', 'agenda']}
+                
+            />
         </div>
     )
 }
