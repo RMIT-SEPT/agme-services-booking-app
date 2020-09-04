7dbe5f59b94ae5306dc6cb4a3073c85b36a25a5e
Duc <duc.h@hotmail.com>
2020-09-04 15:12:26 +1000

    connects to local backend for login and user details on home

--- FrontEnd/src/pages/home.js
+++ FrontEnd/src/pages/home.js
@@ -9,13 +9,26 @@ import PastAppointments from '../components/pastAppointments.js';
 import BookingPage from '../components/bookingPage';
 import ProfilePage from './profile';
 
-const Home = (response) => {
+const Home = async (body) => {
+    // Successful login, get user details from backend.
+    const response = await fetch('http://localhost:8080/api/v1/customer/profile', {
+        method: 'GET',
+        headers: {
+            'Accept': '*/*',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }}).then(response => {
+        if (response.ok) {
+            response.json().then(json => {
+                alert(JSON.stringify(json));
+            })
+        }
+    });
+
     // Probably doesn't need to use state, since the user details don't change on this page.
-    const [userDetails, setUserDetails] = useState(response.location.state);
-    const userName = userDetails.userName;
-    const address = userDetails.address;
-    const userType = userDetails.userType;
-    const token = userDetails.token;
+    const [userDetails, setUserDetails] = useState();
+    // const userName = userDetails.username;
+    // const address = userDetails.address;
+    const userType = "customer";
 
     const [toggleView, setToggleView] = useState("Home")
     const loadHomeMainView = () => {
