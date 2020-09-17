966ea814742dd99e944a9a8ebb5d3aa86f3a5d33
villagewitch <adamhoogwerf@outlook.com.au>
2020-09-17 11:42:50 +1000

    Added base runner script, Added Dockerfile for backend, Added basic kube struct

---
+++ .deployment/runner.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+docker-compose down;
+
+cd ..;
+cd BackEnd;
+
+git pull;
+
+git checkout release;
+
+echo "Creating new backend image"
+mvn spring-boot:build-image -Dspring-boot.build-image.imageName=sept/backend -Dspring.profiles.active=test;
+
+echo "Running backend & postgres containers"
+
+cd ..;
+cd .deployment;
+docker-compose up -d;
+
+# echo "Running frontend container";
+
+
+
